aee5f4baedcb1cf2a05d0202e704706d238a5caa
maylilyo <maylily_o@naver.com>
2019-07-19 15:13:29 +0900

    .

--- sources/crawling+place.py
+++ sources/crawling+place.py
@@ -4,32 +4,51 @@ import re
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from find_name import kakao_log_to_nouns
 
 
-def set_data(line):
-    data_divide = line.split('/', maxsplit=3)  # 시간 / 이름+내용 분리
-    name = data_divide[0]  # 분리된 시간 값을 time 변수에 저장
-    time = findtime(name)
-    place = findplace(name)  # 장소
-    tag = data_divide[3]
-    word = [name, place, time, tag]  # 이름-시간-내용에 맞춰서 한 배열로 정리
-    return word  # 정리된 형식으로 return
+def set_data(name):
+    name = findname(name)  # 분리된 시간 값을 time 변수에 저장
+    if name != '':
+        time_list = findtime(name)
+        place_list = findplace(name)  # 장소
+        # tag = data_divide[3]
+        time = ', '.join(time_list)
+
+        new_address = place_list[0]
+        old_address = place_list[1]
+
+        word = [name, old_address, new_address, time]  # 이름-시간-내용에 맞춰서 한 배열로 정리
+        print("word: "+str(word))
+        return word  # 정리된 형식으로 return
+    else:
+        return ''
 
 
-def text_export(text_name):
+def text_export(names):
     place_data = []
-    file = open(text_name, 'r', encoding='utf-8-sig')
-    lines = file.read().split('\n')
-    for line in lines[:-1]:
-        data = set_data(line)
-        place_data.append(data)
+    for name in names:
+        data = set_data(name)
+        if data != '':
+            place_data.append(data)
 
-    data = pd.DataFrame(place_data)
-    data.columns = ['식당명', '위치', '영업시간', '태그']
-    data = data.set_index("식당명")
+    if place_data != []:
+        data = pd.DataFrame(place_data)
+        data.columns = ['식당명', '지번', '위치', '영업시간']
+        data = data.set_index("식당명")
 
-    data.to_csv('ADE_place.csv', encoding='euc-kr')
+        data.to_csv('../datas/ADE_place.csv', encoding='euc-kr')
 
+def findname(search):
+    url = "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=" + search
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, "html.parser")
+    par_restaurants = soup.findAll("a", attrs={"class": "biz_name"})
+
+    for line in par_restaurants:
+        name =  line.get_text()
+        return name
+    return ''
 
 def findplace(search):
     url = "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=" + search
@@ -66,51 +85,9 @@ def findtime(search):
 
 
 def main():
-    text_export("tagdata.txt")
-
-    names = kakao_log_to_nouns("백화양곱창도 좋아요!")
+    names = kakao_log_to_nouns("호호미욜도 좋아요!")
     print(names)
-
-
-
-processor = TwitterKoreanProcessor(normalization=False, stemming=False)
-hangul = re.compile("[^ 0-9가-힣]+")
-
-
-def kakao_log_to_nouns(sentence):
-
-    names = set()  # 찾은 이름을 저장할 set
-    line = hangul.sub(" ", sentence)
-
-    # 1단계 : 띄어쓰기 기준
-    words = line.split()
-    for word in words:
-        tmp = hangul.sub(" ", word).split()
-        for word in tmp:
-            names.add(word)
-
-    # 2단계 : 명사 기준
-    # example : [KoreanToken(text='준비된', pos='Verb', unknown=False), ... ]
-    tokens = processor.tokenize(line)
-
-    for token in tokens:
-        if token[1] == "Noun":
-            names.add(token[0])
-
-    # 3단계 : 조사 기준
-    for word in words:
-        tokens = processor.tokenize(word)
-
-        tmp = ""
-        for token in tokens:
-            if token[1] != "Josa":
-                tmp += token[0]
-
-        names.add(tmp)
-
-    return list(names)
-
-
+    text_export(names)
 
 if __name__ == '__main__':
     main()
