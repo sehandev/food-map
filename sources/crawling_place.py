aa8ac940546a81573743ac49efae95edaf3ef28c
sehandev <sehandev@gmail.com>
2019-08-16 16:34:57 +0900

    EDIT : optimization

--- sources/crawling_place.py
+++ sources/crawling_place.py
@@ -5,40 +5,43 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from sources import find_tag
-# import find_tag
 
-def set_data(name, place_list, i):
-    # [[식당명, 카테고리, 질문+답변 문장, address][][][][]] (날아옴)
+
+def set_data(place_list):
+    name = place_list[0]
     if name != '':
-        time_list = findtime(name)
+        url = "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=" + name
+        html = requests.get(url)
+        soup = BeautifulSoup(html.text, "html.parser")
+
+        time_list = findtime(soup)
         if time_list == []:
             time_list = " "
-        find_places_list = findplace(name)  # 장소
+        find_places_list = findplace(soup)  # 장소
         # tag = data_divide[3]
         time = ', '.join(time_list)
         new_address = place_list[3]
         if len(find_places_list) == 2:
             old_address = find_places_list[1]
-        else : old_address = " "
+        else:
+            old_address = " "
 
         tag = find_tag.tag_list(place_list[2], place_list[1])
 
         # 카테고리
 
-        word = [name, old_address, new_address, time, tag]  # 이름-시간-내용에 맞춰서 한 배열로 정리
+        word = [name, old_address, new_address,
+                time, tag]  # 이름-시간-내용에 맞춰서 한 배열로 정리
         return word  # 정리된 형식으로 return
     else:
         return ''
 
 
-def text_export(place_list):
-    names = []
-    for i in range(0, len(place_list)):
-        names.append(place_list[i][0])
+def text_export(place_lists):
+    # [[식당명, 카테고리, 질문+답변 문장, address][][][][]] (날아옴)
     place_data = []
-    for name in names:
-        i = names.index(name)
-        data = set_data(name, place_list[i], i)
+    for place_list in place_lists:
+        data = set_data(place_list)
         if data != '':
             place_data.append(data)
 
@@ -47,14 +50,13 @@ def text_export(place_list):
         data.columns = ['식당명', '지번', '위치', '영업시간', '태그']
         data = data.set_index("식당명")
 
-        data.to_csv('./results/ADE_place.csv', encoding='euc-kr')
+        writer = pd.ExcelWriter(
+            './results/ADE_place.xlsx', engine='xlsxwriter')
+        data.to_excel(writer, sheet_name='Sheet1')
+        writer.save()
 
-def findplace(search):
-    url = "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=" + search
 
-    html = requests.get(url)
-    soup = BeautifulSoup(html.text, "html.parser")
-
+def findplace(soup):
     par_place = soup.findAll("span", attrs={"class": "addr"})
     place = []
 
@@ -65,11 +67,7 @@ def findplace(search):
     return place
 
 
-def findtime(search):
-    url = "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=" + search
-
-    html = requests.get(url)
-    soup = BeautifulSoup(html.text, "html.parser")
+def findtime(soup):
     # par_restaurants = soup.findAll("a", attrs={"class": "biz_name"}
     par_time = soup.findAll("span", attrs={"class": "time"})
     #
@@ -85,9 +83,10 @@ def findtime(search):
 
 
 def main():
-    #[[식당명, 카테고리, 질문+답변 문장][][][][]] (날아옴)
+    # [[식당명, 카테고리, 질문+답변 문장][][][][]] (날아옴)
     # place_list = []
     text_export(place_list)
+
 
 if __name__ == '__main__':
     main()
