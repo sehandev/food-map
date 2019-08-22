57d768e5dd1962a3af0e28a784ac504e6bde3cf4
maylilyo <maylily_o@naver.com>
2019-08-22 15:37:08 +0900

    duplicate remove

--- sources/text_export.py
+++ sources/text_export.py
@@ -4,8 +4,9 @@ def question_data(question_data_list):
     question_user_data = []
     for i in range(0, len(question_data_list)):
         parse = question_data_list[i]
-        question_user_data.append(parse)
-
+        if question_data_list[i][2] != question_data_list[i-1][2]:
+            question_user_data.append(parse)
+        
     question_user_data.sort(key=lambda x: x[0])
     name = question_user_data[0][0]
     for i in range(1, len(question_user_data)):  # 중복된 이름이면 지워주고 넘기는 것
@@ -19,9 +20,11 @@ def answer_data(answer_data_list):
     answer_user_data = []
     for i in range(0, len(answer_data_list)):
         parse = answer_data_list[i]
-        answer_user_data.append(parse)
-
+        if answer_data_list[i][2] != answer_data_list[i-1][2]:
+            answer_user_data.append(parse)
+    
     answer_user_data.sort(key=lambda x: x[0])
+    print(answer_user_data)
     name = answer_user_data[0][0]
     for i in range(1, len(answer_user_data)):  # 중복된 이름이면 지워주고 넘기는 것
         if name in answer_user_data[i][0]:
@@ -46,5 +49,5 @@ def text_export(QNA, user_data):
 
 
 if __name__ == '__main__':
-    question_data(질문데이터)
-    answer_data(답변데이터)
+    question_data(question_data)
+    answer_data(answer_data)
